23ceeaa65e8100aa9c0cc91fb3052e1f049d7987
Nikolas Laskaris <laskarisnik@gmail.com>
2026-01-13 10:46:45 -0500

    remove .next from git tracking

--- .next/static/chunks/app_dashboard_page_tsx_a556969c._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/components_map_MapView_tsx_90e4d505._.js",
-  "static/chunks/node_modules_53f4c67f._.js",
-  "static/chunks/_1a7736db._.js"
-],
-    source: "dynamic"
-});
